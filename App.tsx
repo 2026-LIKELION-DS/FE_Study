fedd57d7ee065b69ee9fe9ce4b5d5bbc205d509b
chawon0429 <chawon0429@gmail.com>
2026-09-05 16:30:39 +0900

    chore: 초기 환경 세팅 및 기본 프로젝트 구조 생성

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div className="todo-container">
+      <h1 className="todo-container__header">To-Do List</h1>
+    </div>
+  );
+};
+
+export default App;
