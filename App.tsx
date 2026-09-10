9016c77c555be4d1ecaf2061fe58f8323a2ecf2f
chawon0429 <chawon0429@gmail.com>
2026-09-10 16:16:55 +0900

    feat: TodoInput 컴포넌트 구현

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { TodoInput } from './components/TodoInput';
+import './style.css';
 
 export const App = () => {
+  // TodoInput 잘되는지 테스트
+  const handleAddTodo = (text: string) => {
+    console.log('입력받은 내용:', text);
+    alert(`입력 성공!: ${text}`);
+  }
   return (
     <div className="todo-container">
-      <h1 className="todo-container__header">To-Do List</h1>
+      <h1 className="todo-container__header">🦁LIKELION TO-DO</h1>
+      <TodoInput onAdd={handleAddTodo}/>
     </div>
   );
 };
