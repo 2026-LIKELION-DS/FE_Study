4749eb3253f2510007cbc9cf5ea1392e0df9ce0c
chawon0429 <chawon0429@gmail.com>
2026-09-14 18:48:25 +0900

    feat: TodoList 컴포넌트 구현

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoInput } from './components/TodoInput';
-import { TodoItem } from './components/TodoItem';
+import { TodoList } from './components/TodoList';
 import './style.css';
 
 export interface Todo{
@@ -8,32 +8,47 @@ export interface Todo{
   text: string;
   isDone: boolean;
 }
-export const App = () => {
-  // TodoInput 잘되는지 테스트
+export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
   const handleAddTodo = (text: string) => {
-    console.log('입력받은 내용:', text);
-    alert(`입력 성공!: ${text}`);
-  }
-  // TodoItem 임의데이터
-  const dummyWorkigTodo: Todo = {id:1, text: '공부하기', isDone: false};
-  const dummyDoneTodo: Todo = {id:2, text: '스터디하기', isDone: true};
-  // 버튼 확인
-  const handleToggle = (id:number) => {
-    alert(`완료 버튼 클릭 (ID: ${id})`);
+    const newTodo: Todo = {
+      id: Date.now(),
+      text,
+      isDone: false,
+    };
+    setTodos((prev) => [...prev, newTodo]);
+  };
+  const handleToggle = (id: number) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
   };
   const handleDelete = (id: number) => {
-    alert(`삭제 버튼 클릭 (ID: ${id})`);
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
   };
+  const workingTodos = todos.filter((todo) => !todo.isDone);
+  const doneTodos = todos.filter((todo) => todo.isDone);
   return (
     <div className="todo-container">
       <h1 className="todo-container__header">🦁LIKELION TO-DO</h1>
       <TodoInput onAdd={handleAddTodo}/>
-      <div>
-        <ul className="render-container__list">
-          <TodoItem todo={dummyWorkigTodo} onToggle={handleToggle} onDelete={handleDelete}/>
-          <TodoItem todo={dummyDoneTodo} onToggle={handleToggle} onDelete={handleDelete}/>
-        </ul>
-      </div>
+      <div className="render-container">
+        <TodoList
+            title="할 일"
+            todos={workingTodos}
+            onToggle={handleToggle}
+            onDelete={handleDelete}
+          />
+          <TodoList
+            title="완료"
+            todos={doneTodos}
+            onToggle={handleToggle}
+            onDelete={handleDelete}
+          />
+        </div>
     </div>
   );
 };
